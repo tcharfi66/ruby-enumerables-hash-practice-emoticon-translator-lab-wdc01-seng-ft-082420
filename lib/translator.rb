528568eade7d1950ba92671ebebb7d274e1b5f07
Tej Charfi <charfi@usc.edu>
2020-08-21 15:33:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,8 +3,8 @@ def load_library(yaml_file)
     response = {"get_meaning" => {}, "get_emoticon" => {}}
     library = YAML.load_file(yaml_file)
     library.each do |trans, emos|
-      response["get_meaning"][emos[1]] = trans
-      response["get_emoticon"][emos[0]] = emos[1]
+      response["get_meaning"]emos[1] = trans
+      response["get_emoticon"]emos[0] = emos[1]
     end
     response
 end
