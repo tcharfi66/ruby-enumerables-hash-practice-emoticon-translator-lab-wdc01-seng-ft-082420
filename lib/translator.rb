353320af35e63ec9d4325f4d9732d4295635641a
Tej Charfi <charfi@usc.edu>
2020-08-21 15:48:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,25 +6,28 @@ def load_library(emoticon_files)
       emoticons.each do |key, value|
         new_hash[key] = {english: value[0], japanese: value[1]}
       end
-       new_hash
-       end
+      new_hash
+end
 
 def get_japanese_emoticon(emoticon_files, emoticon)
   emoticons = load_library(emoticon_files)
+  
   emoticons.each do |key, value|
     if emoticon == value[:english]
       return value[:japanese]
+    end
   end
-end
-return "Sorry, that emoticon was not found"
+  
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(emoticon_files, emoticon)
   emoticons = load_library(emoticon_files)
+  
   emoticons.each do |key, value| 
     if emoticon == value[:japanese]
       return key
+    end
   end
-end
-return "Sorry, that emoticon was not found"
+  return "Sorry, that emoticon was not found"
 end
